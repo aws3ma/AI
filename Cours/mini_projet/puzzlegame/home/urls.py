8b59ceba3f7cdef54499bae5751e95f619897225
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-03-14 11:31:05 +0100

    Agent API running correctly finally

--- Cours/mini_projet/puzzlegame/home/urls.py
+++ Cours/mini_projet/puzzlegame/home/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('AI',views.get_result_from_AI,name='getai'),
-    path('ai',views.get_data_to_ai,name='postai'),
+    path('ai',views.post_data_to_ai,name='postai'),
 ]
--- Cours/mini_projet/puzzlegame/home/views.py
+++ Cours/mini_projet/puzzlegame/home/views.py
@@ -1,43 +1,44 @@
 from ast import Try
 from django.shortcuts import render
-# Create your views here.
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-br=[]
-def index(request):
-    DIRECTIONS = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
-    response = json.dumps([{}])
-    return HttpResponse(response,content_type="text/json")
+from .Aiscript import Taquin
 
+br = []
+t = None
+
+
+def index(request):
+
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type="text/json")
+
+#get the result from the agent then send it to the client (frontend)
 def get_result_from_AI(request):
 
     if(request.method == "GET"):
-        try:
-            response = json.dumps([{'table':br}])
-        except:
-            response = json.dumps([{'msg':'error'}])
-    # if(request.method == "POST"):
-    #     # try:
-    #         # print(request.body)
-    #     data=json.loads(request.body)
-    #     a00=data["a00"]
-    #     a01=data["a01"]
-    #     response = json.dumps([{'a00':a00,'a01':a01}])
-    #     # response=json.dumps([{"msg":data["a01"]}])
-    #     # except:
-    #         # response = json.dumps([{'msg':"data"}])
-    
-    return HttpResponse(response,content_type="text/json")
+        b = executeAI(br)
+        response = json.dumps([{'table': b}])
 
+    return HttpResponse(response, content_type="text/json", status=200)
+
+#run the agent code
+def executeAI(br):
+    t = Taquin(br)
+    br = t.main()
+    return br
+
+#get data from client to handle it by the agent
 @csrf_exempt
-def get_data_to_ai(request):
+def post_data_to_ai(request):
     if(request.method == "POST"):
-        data=json.loads(request.body)
-        for i in range(3):
-            l=[]
-            for j in range(3):
-                l.append(int(data["a"+str(i)+str(j)]))
+        data = json.loads(request.body)
+        n = data["n"]
+        for i in range(n):
+            l = []
+            for j in range(n):
+                l.append(int(data["data"]["a"+str(i)+str(j)]))
             br.append(l)
-        response=json.dumps([{"table":br}])
-    return HttpResponse(response,content_type="text/json")
+
+    return HttpResponse(status=200)
